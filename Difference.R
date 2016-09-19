db7a4e4c75bcb00dc16c9eb9e6fab63908fb17b2
Corcoran Barrios <corcoranbarriosd@missouri.edu>
2016-09-19 12:05:06 -0500

    <Function>

--- Difference.R
+++ Difference.R
@@ -1,18 +1,18 @@
+library(rjson)
+library(grid)
+library(gridExtra)
+library(png)
+library(RCurl)
+library(ggplot2)
+library(jpeg)
+library(hexbin)
+library(sp)
+library(knitr)
+library(raster)
+library(rasterVis)
+library(dplyr)
 
-BostonOff <- filter(shotDataTotal2016, TEAM_NAME == "Boston Celtics")
-
-SAOff <- filter(shotDataTotal2016, TEAM_NAME == "San Antonio Spurs")
-
-BostonDef<- shotDatafDef2016[[2]]
-
-SADef <- shotDatafDef2016[[23]]
-
-## find the bounds for the complete data 
-xbnds <- range(c(shotDataTotal2016$LOC_X, BostonDef$LOC_X))
-ybnds <- range(c(shotDataTotal2016$LOC_Y, BostonDef$LOC_Y))
-nbins <- 40
-
-#  function to make a data.frame for geom_hex that can be used with stat_identity
+#function to make the hexbin dataframe
 makeHexData <- function(df) {
   h <- hexbin(df$LOC_X, df$LOC_Y, nbins, xbnds = xbnds, ybnds = ybnds, IDs = TRUE)
   data.frame(hcell2xy(h),
@@ -20,40 +20,71 @@ makeHexData <- function(df) {
              ST = tapply(df$SHOT_MADE_FLAG, h@cID, FUN = function(z) length(z)),
              cid = h@cell)
 }
-#make dataframes of the hex with percentages
-Totalhex <- makeHexData(shotDataTotal2016)
-BostonDefhex <- makeHexData(BostonDef)
-SAOffhex <- makeHexData(SAOff)
 
-##  not all cells are present in each binning, we need to merge by cellID
-BostonDeffbyCell <- merge(Totalhex, BostonDefhex, by = "cid", all = T)
-SAOffByCell <- merge(Totalhex, SAOffhex, by = "cid", all = T)
+#shot Comparison function
 
+ShotComparison <- function(OffTeam, DefTown, SeasondataOff, SeasonDataDef, nbins = 40) {
+  #Filter the offensive data of the Offensive Team
+  Off <- filter(SeasondataOff, TEAM_NAME == OffTeam)
+  #Filter the Deffensive data of the Defensive team
+  deff <- SeasonDataDef[names(SeasonDataDef) == DefTown][[1]]
+  #Get the maximum and minumum values for x and y
+  xbnds <- range(c(SeasondataOff$LOC_X, deff$LOC_X))
+  ybnds <- range(c(SeasondataOff$LOC_Y, deff$LOC_Y))
+  #Make hexbin dataframes out of the teams
+  ##Total NBA data
+  Totalhex <- makeHexData(SeasondataOff)
+  ##Defensive team data
+  Defhex <- makeHexData(deff)
+  ##Offensive team data
+  Offhex <- makeHexData(Off)
+  #Merge offensive and deffensive data with total data by Cell id
+  DeffbyCell <- merge(Totalhex, Defhex, by = "cid", all = T)
+  OffByCell <- merge(Totalhex, Offhex, by = "cid", all = T)
+  ##  when calculating the difference empty cells should count as 0
+  DeffbyCell$PPS.x[is.na(DeffbyCell$PPS.x)] <- 0
+  DeffbyCell$PPS.y[is.na(DeffbyCell$PPS.y)] <- 0
+  DeffbyCell$ST.y[is.na(DeffbyCell$ST.y)] <- 0
 
-##  when calculating the difference empty cells should count as 0
-BostonDeffbyCell$PPS.x[is.na(BostonDeffbyCell$PPS.x)] <- 0
-BostonDeffbyCell$PPS.y[is.na(BostonDeffbyCell$PPS.y)] <- 0
+  OffByCell$PPS.x[is.na(OffByCell$PPS.x)] <- 0
+  OffByCell$PPS.y[is.na(OffByCell$PPS.y)] <- 0
+  OffByCell$ST.y[is.na(OffByCell$ST.y)] <- 0
+  #  make a "difference" data.frame
+  DiffDeff <- data.frame(x = ifelse(is.na(DeffbyCell$x.x), DeffbyCell$x.y, DeffbyCell$x.x),
+                        y = ifelse(is.na(DeffbyCell$y.x), DeffbyCell$y.y, DeffbyCell$y.x),
+                        PPS= DeffbyCell$PPS.y - DeffbyCell$PPS.x,
+                        cid= DeffbyCell$cid, 
+                        ST = DeffbyCell$ST.y)
+  
+  DiffOff <- data.frame(x = ifelse(is.na(OffByCell$x.x), OffByCell$x.y, OffByCell$x.x),
+                        y = ifelse(is.na(OffByCell$y.x), OffByCell$y.y, OffByCell$y.x),
+                        PPS= OffByCell$PPS.y - OffByCell$PPS.x,
+                        ST = OffByCell$ST.x,
+                        cid = OffByCell$cid, 
+                        ST = OffByCell$ST.y)
+  #make team comparisons
+  Comparison <- merge(DiffOff, DiffDeff, by = "cid", all = T) 
+  Comparison <- Comparison[,-c(6:7)]
+  Comparison$Diff <- c(Comparison$PPS.x + Comparison$PPS.y)
+  
+  
+  PPSAA <- weighted.mean((Comparison$PPS.x + Comparison$PPS.y), Comparison$ST.x)
+  
+  OFF <- ggplot(DiffOff)  + 
+    annotation_custom(court, -250, 250, -52, 418) +
+    geom_hex(aes(x = x, y = y, fill = PPS),
+             stat = "identity", alpha = 0.8) +
+    guides(alpha = FALSE, size = FALSE) +
+    coord_fixed()  +theme(line = element_blank(),
+                          axis.title.x = element_blank(),
+                          axis.title.y = element_blank(),
+                          axis.text.x = element_blank(),
+                          axis.text.y = element_blank(),
+                          legend.title = element_blank(),
+                          plot.title = element_text(size = 17, lineheight = 1.2, face = "bold")) + ggtitle(Paste("Offensive\n Shot Chart")) + scale_fill_gradient2(name="Off PPS")
+  
+  return(list(Off = DiffOff, deff = DiffDeff, Comparison = Comparison, Total = Totalhex, PPSAA = PPSAA))
+}
 
-SAOffByCell$PPS.x[is.na(SAOffByCell$PPS.x)] <- 0
-SAOffByCell$PPS.y[is.na(SAOffByCell$PPS.y)] <- 0
-SAOffByCell$ST.y[is.na(SAOffByCell$ST.y)] <- 0
+str(ShotComparison(OffTeam = "Boston Celtics", DefTown = "San Antonio", SeasondataOff = shotDataTotal2016, SeasonDataDef = shotDatafDef2016, nbins = 40))
 
-
-##  make a "difference" data.frame
-BostonDiffDeff <- data.frame(x = ifelse(is.na(BostonDeffbyCell$x.x), BostonDeffbyCell$x.y, BostonDeffbyCell$x.x),
-                   y = ifelse(is.na(BostonDeffbyCell$y.x), BostonDeffbyCell$y.y, BostonDeffbyCell$y.x),
-                   PPS= BostonDeffbyCell$PPS.y - BostonDeffbyCell$PPS.x,
-                   cid= BostonDeffbyCell$cid)
-
-SADiffOff <- data.frame(x = ifelse(is.na(SAOffByCell$x.x), SAOffByCell$x.y, SAOffByCell$x.x),
-                             y = ifelse(is.na(SAOffByCell$y.x), SAOffByCell$y.y, SAOffByCell$y.x),
-                             PPS= SAOffByCell$PPS.y - SAOffByCell$PPS.x,
-                             ST = SAOffByCell$ST.x,
-                             cid = SAOffByCell$cid)
-
-Comparison <- merge(SADiffOff, BostonDiffDeff, by = "cid", all = T) 
-Comparison <- Comparison[,-c(6:7)]
-Comparison$Diff <- c(Comparison$PPS.x + Comparison$PPS.y)
-
-
-weighted.mean((Comparison$PPS.x + Comparison$PPS.y), Comparison$ST)
