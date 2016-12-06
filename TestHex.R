5dc1c7adc7583c63e0c00fe422b6c6c35714a909
Corcoran Barrios <corcoranbarriosd@missouri.edu>
2016-12-06 11:09:07 -0600

    <Offense hexagons>

--- TestHex.R
+++ TestHex.R
@@ -72,6 +72,7 @@ ShotComparisonGraph <- function(OffTeam, DefTown, SeasondataOff, SeasonDataDef, 
   
   PPSAA <- weighted.mean((Comparison$PPS.x + Comparison$PPS.y), Comparison$ST.x)
   
+  
   #Legend extractor
   g_legend <- function(a.gplot){
     tmp <- ggplot_gtable(ggplot_build(a.gplot))
@@ -79,30 +80,49 @@ ShotComparisonGraph <- function(OffTeam, DefTown, SeasondataOff, SeasonDataDef, 
     legend <- tmp$grobs[[leg]]
     return(legend)}
   
-  OFFLEG <- ggplot(DiffOff) + annotation_custom(court, -250, 250, -52, 418) + geom_point(aes(x = x, y = y, color = PPS, size = ST), stat = "identity") +
-    coord_fixed()  +theme(line = element_blank(),
-                          axis.title.x = element_blank(),
-                          axis.title.y = element_blank(),
-                          axis.text.x = element_blank(),
-                          axis.text.y = element_blank(),
-                          legend.title = element_blank(),
-                          plot.title = element_text(size = 17, lineheight = 1.2, face = "bold"))+ scale_size(range = c(0, maxsize)) +scale_color_gradient2(low = "blue", high = "red", name = "APPS") + ylim(c(-51, 400))+ theme(legend.position="bottom") +  ggtitle(paste(OffTeam, "Offensive\n Shot Chart", sep = " "))
-  leg<-g_legend(OFFLEG)
+  #Function to transform hexbins into polygons
+  hex_coord_df <- function(x, y, width, height, size = 1) {
+    # like hex_coord but returns a dataframe of vertices grouped by an id variable
+    dx <- log(size * width / 6)
+    dy <- log(size * height / 2 / sqrt(3))
+    
+    hex_x <- rbind(x - 2 * dx, x - dx, x + dx, x + 2 * dx, x + dx, x - dx)
+    hex_y <- rbind(y, y + dy, y + dy, y, y - dy, y - dy)
+    id    <- rep(1:length(x), each=6)
+    
+    data.frame(cbind(x=as.vector(hex_x), y=as.vector(hex_y), id))
+  }
+  
+  #Filter by quantile
   
   DiffOff <- filter(DiffOff, ST > quantile(DiffOff$ST, probs = quant))
   DiffDeff <- filter(DiffDeff, ST > quantile(DiffDeff$ST, probs = quant))
   Comparison <- filter(Comparison, ST.x > quantile(Comparison$ST.x, probs = quant))
   
+  #Transform Hexbins into polygons
   
+  DFOFF <- hex_coord_df(DiffOff$x, DiffOff$y, (0.05*DiffOff$ST), (0.05*DiffOff$ST), size =1)
+  DFOFF$PPS <- rep(DiffOff$PPS, each = 6)
   
-  OFF <- ggplot(DiffOff) + annotation_custom(court, -250, 250, -52, 418) + geom_point(aes(x = x, y = y, color = PPS, size = ST), stat = "identity") +
+  #Create Legend
+  OFFLEG <- ggplot(DFOFF, aes(x=x, y = y))+ annotation_custom(court, -250, 250, -52, 418) + geom_polygon(aes(group = id, fill = PPS)) + scale_fill_gradient2(low ="blue", high = "red", limits=c(-1.2, 1.2)) +
     coord_fixed()  +theme(line = element_blank(),
                           axis.title.x = element_blank(),
                           axis.title.y = element_blank(),
                           axis.text.x = element_blank(),
                           axis.text.y = element_blank(),
                           legend.title = element_blank(),
-                          plot.title = element_text(size = 17, lineheight = 1.2, face = "bold"))+ scale_size(range = c(0, maxsize)) + scale_color_gradient2(low = "blue", high = "red", name = "APPS") + ylim(c(-51, 400))+ theme(legend.position="none") +  ggtitle(paste(OffTeam, "Offensive\n Shot Chart", sep = " "))
+                          plot.title = element_text(size = 17, lineheight = 1.2, face = "bold"))+ scale_size(range = c(0, maxsize)) + ylim(c(-51, 400))+ theme(legend.position="bottom") +  ggtitle(paste(OffTeam, "Offensive\n Shot Chart", sep = " "))
+  leg<-g_legend(OFFLEG)
+  
+  OFF <- ggplot(DFOFF, aes(x=x, y = y))+ annotation_custom(court, -250, 250, -52, 418) + geom_polygon(aes(group = id, fill = PPS)) + scale_fill_gradient2(low ="blue", high = "red", limits=c(-1.2, 1.2)) +
+    coord_fixed()  +theme(line = element_blank(),
+                          axis.title.x = element_blank(),
+                          axis.title.y = element_blank(),
+                          axis.text.x = element_blank(),
+                          axis.text.y = element_blank(),
+                          legend.title = element_blank(),
+                          plot.title = element_text(size = 17, lineheight = 1.2, face = "bold"))+ scale_size(range = c(0, maxsize)) + ylim(c(-51, 400))+ theme(legend.position="none") +  ggtitle(paste(OffTeam, "Offensive\n Shot Chart", sep = " "))
   DEF <- ggplot(DiffDeff)  + annotation_custom(court, -250, 250, -52, 418) + geom_point(aes(x = x, y = y, color = PPS, size = ST), stat = "identity") +
     coord_fixed()  +theme(line = element_blank(),
                           axis.title.x = element_blank(),
@@ -126,7 +146,7 @@ ShotComparisonGraph <- function(OffTeam, DefTown, SeasondataOff, SeasonDataDef, 
   return(list(Off = DiffOff, deff = DiffDeff, Comparison = Comparison, Total = Totalhex, PPSAA = PPSAA, p = p, leg = leg, charts = charts))
 }
 
-Com1 <- ShotComparisonGraph(OffTeam = "Philadelphia 76ers", DefTown = "San Antonio", SeasondataOff = shotDataTotal2016, SeasonDataDef = shotDatafDef2016, nbins = 30, quant = 0.7)
+Com1 <- ShotComparisonGraph(OffTeam = "Golden State Warriors", DefTown = "Portland", SeasondataOff = shotDataTotal2016, SeasonDataDef = shotDatafDef2016, nbins = 30, quant = 0.7)
 Com2 <- ShotComparisonGraph(OffTeam = "Cleveland Cavaliers", DefTown = "Golden State", SeasondataOff = shotDataTotal2016, SeasonDataDef = shotDatafDef2016, nbins = 30, quant = 0.7)
 
 grid.arrange(Com1$charts,Com2$charts,Com1$leg,ncol=1,heights=c(3/7, 3/7 ,1/7))
@@ -143,21 +163,38 @@ hex_coord_df <- function(x, y, width, height, size = 1) {
   data.frame(cbind(x=as.vector(hex_x), y=as.vector(hex_y), id))
 }
 
-DFOFF <- hex_coord_df(Com1$Off$x, Com1$Off$y, Com1$Off$ST.1, Com1$Off$ST.1, size =1)
+DFOFF <- hex_coord_df(Com1$Off$x, Com1$Off$y, (0.05*Com1$Off$ST), (0.05*Com1$Off$ST), size =1)
 DFOFF$PPS <- rep(Com1$Off$PPS, each = 6)
-DFOFF$color <- rep(ifelse(Com1$Off$PPS <= -0.5, "blue",ifelse(Com1$Off$PPS > -0.5 & Com1$Off$PPS < -0.25, "cyan", ifelse(Com1$Off$PPS >= -0.25 & Com1$Off$PPS <= 0.25, "gold", ifelse(Com1$Off$PPS > 0.25 &Com1$Off$PPS < 0.5, "orange", "red")))), each = 6)
-ggplot(DFOFF, aes(x=x, y = y))+ annotation_custom(court, -250, 250, -52, 418) + geom_polygon(aes(group = id), fill = DFOFF$color) 
-
+ggplot(DFOFF, aes(x=x, y = y))+ annotation_custom(court, -250, 250, -52, 418) + geom_polygon(aes(group = id, fill = PPS)) + scale_fill_gradient2(low ="blue", high = "red", limits=c(-1, 1))
 
 DFDEF <- hex_coord_df(Com1$deff$x, Com1$deff$y, Com1$deff$ST, Com1$deff$ST, size =1)
 DFDEF$PPS <- rep(Com1$deff$PPS, each = 6)
-DFDEF$color <- rep(ifelse(Com1$deff$PPS <= -0.5, "blue",ifelse(Com1$deff$PPS > -0.5 & Com1$deff$PPS < -0.25, "cyan", ifelse(Com1$deff$PPS >= -0.25 & Com1$deff$PPS <= 0.25, "gold", ifelse(Com1$deff$PPS > 0.25 &Com1$deff$PPS < 0.5, "orange", "red")))), each = 6)
-ggplot(DFDEF, aes(x=x, y = y))+ annotation_custom(court, -250, 250, -52, 418) + geom_polygon(aes(group = id), fill = DFDEF$color)
+ggplot(DFDEF, aes(x=x, y = y))+ annotation_custom(court, -250, 250, -52, 418) + geom_polygon(aes(group = id, fill = PPS))+ scale_fill_gradient2(low ="blue", high = "red", limits=c(-1, 1))
 
-DFDIF <- hex_coord_df(Com1$Comparison$x.x, Com1$Comparison$y.y, Com1$Comparison$ST.y, Com1$Comparison$ST.y, size =1)
+
+####Golden State Attack
+Com1 <- ShotComparisonGraph(OffTeam = "Golden State Warriors", DefTown = "Portland", SeasondataOff = shotDataTotal2016, SeasonDataDef = shotDatafDef2016, nbins = 30, quant = 0.7)
+
+DFDIF <- hex_coord_df(Com1$Comparison$x.x, Com1$Comparison$y.x, (0.05*Com1$Comparison$ST.x),(0.05*Com1$Comparison$ST.x), size =1)
 DFDIF$Dif <- rep(Com1$Comparison$Diff, each = 6)
-DFDIF$color <- rep(ifelse(Com1$Comparison$Diff <= -0.5, "blue",
-               ifelse(Com1$Comparison$Diff > -0.5 & Com1$Comparison$Diff < -0.25, "cyan", 
-               ifelse(Com1$Comparison$Diff >= -0.25 &Com1$Comparison$Diff <= 0.25, "gold", 
-               ifelse(Com1$Comparison$Diff > 0.25 & Com1$Comparison$Diff < 0.5, "orange", "red")))), each = 6)
-ggplot(DFDIF, aes(x=x, y = y))+ annotation_custom(court, -250, 250, -52, 418) + geom_polygon(aes(group = id), fill = DFDIF$color) 
+ggplot(DFDIF, aes(x=x, y = y))+ annotation_custom(court, -250, 250, -52, 418) + geom_polygon(aes(group = id, fill = Dif)) + scale_fill_gradient2(low ="blue", high = "red", limits=c(-1, 1)) + coord_fixed()  +theme(line = element_blank(),
+                        axis.title.x = element_blank(),
+                        axis.title.y = element_blank(),
+                        axis.text.x = element_blank(),
+                        axis.text.y = element_blank(),
+                        legend.title = element_blank(),
+                        plot.title = element_text(size = 17, lineheight = 1.2, face = "bold"))+ scale_size(range = c(0, maxsize)) +scale_color_gradient2(low = "blue", high = "red", name = "APPS") + ylim(c(-51, 400))+ theme(legend.position="bottom") + ylim(c(-26, 265))
+
+
+####Portland Attack
+Com1 <- ShotComparisonGraph(OffTeam = "Portland Trail Blazers", DefTown = "Golden State", SeasondataOff = shotDataTotal2016, SeasonDataDef = shotDatafDef2016, nbins = 30, quant = 0.7)
+
+DFDIF <- hex_coord_df(Com1$Comparison$x.x, Com1$Comparison$y.x, (0.05*Com1$Comparison$ST.x),(0.05*Com1$Comparison$ST.x), size =1)
+DFDIF$Dif <- rep(Com1$Comparison$Diff, each = 6)
+ggplot(DFDIF, aes(x=x, y = y))+ annotation_custom(court, -250, 250, -52, 418) + geom_polygon(aes(group = id, fill = Dif)) + scale_fill_gradient2(low ="blue", high = "red", limits=c(-1, 1)) + coord_fixed()  +theme(line = element_blank(),
+                                                                                                                                                                                                                     axis.title.x = element_blank(),
+                                                                                                                                                                                                                     axis.title.y = element_blank(),
+                                                                                                                                                                                                                     axis.text.x = element_blank(),
+                                                                                                                                                                                                                     axis.text.y = element_blank(),
+                                                                                                                                                                                                                     legend.title = element_blank(),
+                                                                                                                                                                                                                     plot.title = element_text(size = 17, lineheight = 1.2, face = "bold"))+ scale_size(range = c(0, maxsize)) +scale_color_gradient2(low = "blue", high = "red", name = "APPS") + ylim(c(-51, 400))+ theme(legend.position="bottom") + ylim(c(-26, 265))
