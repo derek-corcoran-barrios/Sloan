dfcd0034ca0976b4aa43379685466a0a5d496cee
Corcoran Barrios <corcoranbarriosd@missouri.edu>
2017-02-03 09:44:41 -0600

    <3Feb2017, RWP>

--- WordPress.R
+++ WordPress.R
@@ -2,7 +2,7 @@ if (!require('RWordPress')) {
   devtools::install_github(c("duncantl/XMLRPC", "duncantl/RWordPress"))
 }
 library(RWordPress)
-options(WordpressLogin = c(user = '135792468++'),
+options(WordpressLogin = c(spatialball = '135792468++'),
         WordpressURL = 'https://spatialball.wordpress.com/xmlrpc.php')
 library(knitr)
-knit2wp('Projection.Rmd', title = 'Projections')
+knit2wp('Projection.Rmd', title = 'Projections',  action = c("editPost"), postid = 77)
