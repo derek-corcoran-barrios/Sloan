6a9ec9f024e38d6dde05029d6767016734c3cc8f
derek-corcoran-barrios <gaiterodelsurdelmundo@gmail.com>
2017-04-21 10:35:51 -0300

    <Finally corrected>

--- Corrected.R
+++ Corrected.R
@@ -240,8 +240,218 @@ ggplot(DFDates_c, aes(x = day, y = defAPPS))+ geom_point(aes(color = Season)) + 
 
 saveRDS(DFDates_c, "DFDates_c.rds")
 
-past_gamesFilt <- dplyr::filter(past_games, Date >= for_filtering$dates[1] & Season == 2013 
-                                | Date >= for_filtering$dates[2] & Season == 2014 
-                                | Date >= for_filtering$dates[3] & Season == 2015
-                                | Date >= for_filtering$dates[4] & Season == 2016
-                                | Date >= for_filtering$dates[5] & Season == 2017)
+past_gamesFilt_c <- dplyr::filter(past_games_c, Date >= for_filtering_c$dates[1] & Season == 2013 
+                                | Date >= for_filtering_c$dates[2] & Season == 2014 
+                                | Date >= for_filtering_c$dates[3] & Season == 2015
+                                | Date >= for_filtering_c$dates[4] & Season == 2016
+                                | Date >= for_filtering_c$dates[5] & Season == 2017)
+
+
+past_gamesFilt_c <- past_gamesFilt_c[complete.cases(past_gamesFilt_c),]
+
+
+ggplot(past_gamesFilt, aes(x = HomeRes, y = defAPPS)) + geom_smooth()
+past_gamesFilt$Type <- "regular_season"
+
+
+saveRDS(for_filtering_c, "for_filtering_c.rds")
+saveRDS(past_gamesFilt_c, "past_gamesFilt_c.rds")
+
+FinalOdds <- readRDS("FinalOdds.rds")
+FinalOdds <- dplyr::filter(FinalOdds, Season != 2012)
+
+FinalOdds$Visitor <- gsub("MIN" , "Min", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("LAL", "Lal", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("SAC", "Sac", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("LAC", "Lac", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("POR", "Por", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("PHX", "Pho", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("DAL", "Dal", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("SAS", "Sas", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("SAN", "Sas", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("OKC", "Okc", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("MIL", "Mil", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("NYK", "NY", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("MEM", "Mem", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("DEN", "Den", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("HOU", "Hou", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("IND", "Ind", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("TOR", "Tor", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("CHA", "Cha", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("CHR", "Cha", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("MIA", "Mia", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("UTA", "Uta", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("BKN", "Bkn", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("PHI", "Phi", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("WAS", "Was", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("ORL", "ORL", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("GSW", "GSW", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("NOH", "NO", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("NOP", "NO", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("CLE", "Cle", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("BOS", "Bos", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("CHI", "Chi", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("ATL", "Atl", as.character(FinalOdds$Visitor))
+FinalOdds$Visitor <- gsub("DET", "Det", as.character(FinalOdds$Visitor))
+
+FinalOdds$Home <- gsub("MIN" , "Min", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("LAL", "Lal", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("SAC", "Sac", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("LAC", "Lac", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("POR", "Por", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("PHX", "Pho", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("DAL", "Dal", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("SAS", "Sas", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("SAN", "Sas", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("OKC", "Okc", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("MIL", "Mil", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("NYK", "NY", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("MEM", "Mem", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("DEN", "Den", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("HOU", "Hou", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("IND", "Ind", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("TOR", "Tor", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("CHA", "Cha", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("CHR", "Cha", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("MIA", "Mia", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("UTA", "Uta", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("BKN", "Bkn", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("PHI", "Phi", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("WAS", "Was", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("ORL", "ORL", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("GSW", "GSW", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("NOH", "NO", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("NOP", "NO", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("CLE", "Cle", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("BOS", "Bos", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("CHI", "Chi", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("ATL", "Atl", as.character(FinalOdds$Home))
+FinalOdds$Home <- gsub("DET", "Det", as.character(FinalOdds$Home))
+
+for(i in 1:NROW(FinalOdds)) {
+ if (FinalOdds$Season[i] == 2016){
+    shotDatafDef2016Temp <- shotDatafDef2016
+    shotDatafDef2016Temp[[FinalOdds$Visitor[i]]] <- dplyr::filter(shotDatafDef2016[[FinalOdds$Visitor[i]]], GAME_DATE < FinalOdds$Date[i])
+    FinalOdds$offAPPS[i] <- ComparisonPPSc(OffTeam = FinalOdds$Visitor[i], DefTown = FinalOdds$Home[i], SeasondataOff = dplyr::filter(shotDataTotal2016, GAME_DATE < FinalOdds$Date[i]), SeasonDataDef = shotDatafDef2016Temp)
+  }else if (FinalOdds$Season[i] == 2015){
+    shotDatafDef2015Temp <- shotDatafDef2015
+    shotDatafDef2015Temp[[FinalOdds$Visitor[i]]] <- dplyr::filter(shotDatafDef2015[[FinalOdds$Visitor[i]]], GAME_DATE < FinalOdds$Date[i])
+    FinalOdds$offAPPS[i] <- ComparisonPPSc(OffTeam = FinalOdds$Visitor[i], DefTown = FinalOdds$Home[i], SeasondataOff = dplyr::filter(shotDataTotal2015, GAME_DATE < FinalOdds$Date[i]), SeasonDataDef = shotDatafDef2015Temp)
+  }else if (FinalOdds$Season[i] == 2014 & i >10){
+    shotDatafDef2014Temp <- shotDatafDef2014
+    shotDatafDef2014Temp[[FinalOdds$Visitor[i]]] <- dplyr::filter(shotDatafDef2014[[FinalOdds$Visitor[i]]], GAME_DATE < FinalOdds$Date[i])
+    FinalOdds$offAPPS[i] <- ComparisonPPSc(OffTeam = FinalOdds$Visitor[i], DefTown = FinalOdds$Home[i], SeasondataOff = dplyr::filter(shotDataTotal2014, GAME_DATE < FinalOdds$Date[i]), SeasonDataDef = shotDatafDef2014Temp)
+  }else if (FinalOdds$Season[i] == 2013 & i >20){
+    shotDatafDef2013Temp <- shotDatafDef2013
+    shotDatafDef2013Temp[[FinalOdds$Visitor[i]]] <- dplyr::filter(shotDatafDef2013[[FinalOdds$Visitor[i]]], GAME_DATE < FinalOdds$Date[i])
+    FinalOdds$offAPPS[i] <- ComparisonPPSc(OffTeam = FinalOdds$Visitor[i], DefTown = FinalOdds$Home[i], SeasondataOff = dplyr::filter(shotDataTotal2013, GAME_DATE < FinalOdds$Date[i]), SeasonDataDef = shotDatafDef2013Temp)
+  }else{
+    FinalOdds$offAPPS[i] <- NA
+  }
+}
+
+FinalOdds$defAPPS <- unlist(FinalOdds$defAPPS)
+
+FinalOdds$offAPPS <- unlist(FinalOdds$offAPPS)
+
+FinalOdds_c <- FinalOdds
+saveRDS(FinalOdds_c, "FinalOdds_c.rds")
+
+past_gamesFiltPlayoff_c <- plyr::rbind.fill(past_gamesFilt_c, FinalOdds_c)
+past_gamesFiltPlayoff_c <- dplyr::arrange(past_gamesFiltPlayoff_c, Date)
+saveRDS(past_gamesFiltPlayoff_c, "past_gamesFiltPlayoff_c.rds")
+
+
+##Train model
+
+#Divide in train and test set
+##START FROM HERE
+past_gamesFiltPlayoff_c <- readRDS("past_gamesFiltPlayoff_c.rds")
+
+#Train set playoffs 2013 through 2015 and regular season 2013 through 2016
+trainNBA_c <- dplyr::filter(past_gamesFiltPlayoff_c, Season != 2017 & Type == "regular_season" | Season != 2016 & Type == "Playoffs")
+testNBA_c <- dplyr::filter(past_gamesFiltPlayoff_c, Season == 2017 & Type == "regular_season" | Season == 2016 & Type == "Playoffs")
+
+
+#####Forcast
+
+####Caret version
+library(caret)
+ctrl <- trainControl(method = "repeatedcv", number=10, repeats=10)
+
+
+grid <- expand.grid(interaction.depth = seq(1, 7, by = 2),
+                    n.trees = seq(100, 1000, by = 50),
+                    shrinkage = c(0.01, 0.1),
+                    n.minobsinnode=c(1,5,10))
+
+
+# train the GBM model
+set.seed(7)
+BRT2017_20_Abr_c <- train(x = trainNBA_c[,c(7,8)],y = trainNBA_c[,9], method = "gbm",  preProcess = c("center", "scale"), verbose = TRUE, trControl = ctrl, tuneGrid = grid)
+saveRDS(BRT2017_20_Abr_c, "BRT2017_20_Abr_c.rds")
+
+BRT2017_20_Abr_c <- readRDS("BRT2017_20_Abr_c.rds")
+
+testNBA_c$PredictedBRT <- predict(BRT2017_20_Abr_c, testNBA_c[,7:8])
+
+
+ggplot(testNBA_c, aes(x = HomeRes, y = PredictedBRT)) + geom_smooth() + geom_point() + xlab("Diferencia") + ylab("Diferencia predicha")
+
+################################HASTA ACA!!!!
+
+For.predictions_c <- expand.grid(defAPPS = seq(from = min(past_gamesFiltPlayoff_c$defAPPS), to = max(past_gamesFiltPlayoff_c$defAPPS), length.out = 100), 
+                               offAPPS =seq(from= min(past_gamesFiltPlayoff_c$offAPPS, na.rm = TRUE),to = max(past_gamesFiltPlayoff_c$offAPPS, na.rm = TRUE), length.out = 100))
+
+For.predictions_c$Spread <- predict(BRT2017_20_Abr_c, For.predictions_c)
+
+For.predictions2_c <- For.predictions_c
+For.predictions2_c$Type <- c("Predicted")
+For.predictions3_c <- For.predictions2_c[seq(from =1, to = NROW(For.predictions_c), by = 100),]
+For.predictions3_c$Spread <- 0
+For.predictions3_c$Type <- c("Push")
+For.predictions2_c <- rbind(For.predictions2_c, For.predictions3_c)
+
+#Test 1
+wireframe(Spread ~  offAPPS + defAPPS, group = Type, data = For.predictions2_c, colorkey = TRUE, drape = TRUE, pretty = TRUE,scales = list(arrows = FALSE), screen = list(z = -220, x = -80), par.settings = list(regions=list(alpha=0.75)))
+
+postResample(pred = testNBA_c$PredictedBRT, obs = testNBA_c$HomeRes)
+
+summary(BRT2017_20_Abr_c$resample)
+
+WLtestNBA_c <- testNBA_c
+
+WLtestNBA_c$HomeRes <- as.factor(ifelse(WLtestNBA_c$HomeRes < 0, "W", "L"))
+WLtestNBA_c$PredictedBRT <- as.factor(ifelse(WLtestNBA_c$PredictedBRT < 0, "W", "L"))
+
+confusionMatrix(WLtestNBA_c$PredictedBRT, WLtestNBA_c$HomeRes, positive = "W")
+
+df <- data.frame(matrix(ncol = 30, nrow = 30))
+colnames(df) <- as.character(unique(shotDataTotal2017$TEAM_NAME))
+rownames(df) <- names(shotDatafDef2017)
+ 
+Offensive_teams <- as.character(unique(shotDataTotal2017$TEAM_NAME))
+defenseve_names <- names(shotDatafDef2017)
+
+for (i in 1:length(Offensive_teams)) {
+  for (j in 1:length(defenseve_names)){
+     df[rownames(df) == defenseve_names[j],colnames(df) == Offensive_teams[i]] <- ComparisonPPSc(OffTeam = Offensive_teams[i], DefTown = defenseve_names[j], SeasondataOff = shotDataTotal2017, SeasonDataDef = shotDatafDef2017)
+   }
+}
+
+ROWS <- sort(rownames(df))
+COLS <- sort(colnames(df))
+df2 <- df
+for (i in 1:length(ROWS)) {
+  df[rownames(df) == COLS[i], colnames(df) == ROWS[i]] <- NA 
+}
+
+offrating <- colMeans(df, na.rm = TRUE)*50
+defrating <- rowMeans(df, na.rm = TRUE)*-50
+
+offratingDF <- data.frame(Team = colnames(df), offrating = offrating)
+defratingDF <- data.frame(Team = rownames(df), defrating = defrating)
+
+netDF <- merge.data.frame(offratingDF, defratingDF)
+netDF$netrating <- netDF$offrating + netDF$defrating
+
